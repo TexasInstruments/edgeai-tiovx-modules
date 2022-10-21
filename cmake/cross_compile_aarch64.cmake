TexasInstruments/edgeai-tiovx-modules

commit 42c11c837fdd447160cf2816bfc0a4a4239f304f

cmake: cross-compile: Fix error handling
Do not flag errors when something is not
defined, instead just print a message and
use the default

Signed-off-by: Rahul T R <r-ravikumar@ti.com>

cmake/cross_compile_aarch64.cmake
@@ -3,32 +3,33 @@
 set(CMAKE_SYSTEM_NAME      Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-if (NOT DEFINED ENV{CROSS_COMPILER_PATH})
-    message(FATAL_ERROR "CROSS_COMPILER_PATH not defined.")
+if (DEFINED ENV{CROSS_COMPILER_PREFIX})
+    set(CROSS_COMPILER_PREFIX   $ENV{CROSS_COMPILER_PREFIX})
+else()
+    message("CROSS_COMPILER_PREFIX not defined, Setting to NULL")
 endif()
 
-if (NOT DEFINED ENV{CROSS_COMPILER_PREFIX})
-    message(FATAL_ERROR "CROSS_COMPILER_PREFIX not defined.")
+if (DEFINED ENV{CROSS_COMPILER_PATH})
+    set(CROSS_COMPILER_PATH     $ENV{CROSS_COMPILER_PATH})
+    set(CMAKE_SYSROOT           ${TARGET_FS})
+    set(TOOLCHAIN_PREFIX        ${CROSS_COMPILER_PATH}/bin/${CROSS_COMPILER_PREFIX})
+    set(CMAKE_C_COMPILER        ${TOOLCHAIN_PREFIX}gcc)
+    set(CMAKE_CXX_COMPILER      ${TOOLCHAIN_PREFIX}g++)
+    set(CMAKE_AR                ${TOOLCHAIN_PREFIX}ar CACHE FILEPATH "")
+    set(CMAKE_LINKER            ${TOOLCHAIN_PREFIX}ld)
+    set(CMAKE_OBJCOPY           ${TOOLCHAIN_PREFIX}objcopy)
+    set(CMAKE_RANLIB            ${TOOLCHAIN_PREFIX}ranlib CACHE FILEPATH "")
+    set(CMAKE_SIZE              ${TOOLCHAIN_PREFIX}size)
+    set(CMAKE_STRIP             ${TOOLCHAIN_PREFIX}strip)
+else()
+    message("CROSS_COMPILER_PATH not defined, Using default TOOLCHAIN")
 endif()
 
-if (NOT DEFINED ENV{TARGET_FS})
-    message(FATAL_ERROR "TARGET_FS not defined.")
+if (DEFINED ENV{TARGET_FS})
+    set(TARGET_FS               $ENV{TARGET_FS})
+else()
+    message("TARGET_FS not defined, Using /")
 endif()
-
-set(CROSS_COMPILER_PATH     $ENV{CROSS_COMPILER_PATH})
-set(CROSS_COMPILER_PREFIX   $ENV{CROSS_COMPILER_PREFIX})
-set(TARGET_FS               $ENV{TARGET_FS})
-
-set(CMAKE_SYSROOT           ${TARGET_FS})
-set(TOOLCHAIN_PREFIX        ${CROSS_COMPILER_PATH}/bin/${CROSS_COMPILER_PREFIX})
-set(CMAKE_C_COMPILER        ${TOOLCHAIN_PREFIX}gcc)
-set(CMAKE_CXX_COMPILER      ${TOOLCHAIN_PREFIX}g++)
-set(CMAKE_AR                ${TOOLCHAIN_PREFIX}ar CACHE FILEPATH "")
-set(CMAKE_LINKER            ${TOOLCHAIN_PREFIX}ld)
-set(CMAKE_OBJCOPY           ${TOOLCHAIN_PREFIX}objcopy)
-set(CMAKE_RANLIB            ${TOOLCHAIN_PREFIX}ranlib CACHE FILEPATH "")
-set(CMAKE_SIZE              ${TOOLCHAIN_PREFIX}size)
-set(CMAKE_STRIP             ${TOOLCHAIN_PREFIX}strip)
 
 # search programs, headers and, libraries in the target environment
 # programs: make etc

commit cb3de20d2fb3ce5f82e303f1e7ff68e82a7de80d

cmake: Fix setting the SYSROOT
Set SYSROOT at right place, after
TARGET_FS is set

Fixes: 42c11c8 ("cmake: cross-compile: Fix error handling")
Signed-off-by: Rahul T R <r-ravikumar@ti.com>

cmake/cross_compile_aarch64.cmake
@@ -11,7 +11,6 @@ endif()
 
 if (DEFINED ENV{CROSS_COMPILER_PATH})
     set(CROSS_COMPILER_PATH     $ENV{CROSS_COMPILER_PATH})
-    set(CMAKE_SYSROOT           ${TARGET_FS})
     set(TOOLCHAIN_PREFIX        ${CROSS_COMPILER_PATH}/bin/${CROSS_COMPILER_PREFIX})
     set(CMAKE_C_COMPILER        ${TOOLCHAIN_PREFIX}gcc)
     set(CMAKE_CXX_COMPILER      ${TOOLCHAIN_PREFIX}g++)
@@ -27,6 +26,7 @@ endif()
 
 if (DEFINED ENV{TARGET_FS})
     set(TARGET_FS               $ENV{TARGET_FS})
+    set(CMAKE_SYSROOT           ${TARGET_FS})
 else()
     message("TARGET_FS not defined, Using /")
 endif()